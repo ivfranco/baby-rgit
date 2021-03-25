5d1a1ff9f769c8af2579d4aa4a5a22187bd7259b
ivfranco <ivfranco33@outlook.com>
2021-03-26 01:14:04 +0800

    revert to upper case acronym

--- src/bin/read_tree.rs
+++ src/bin/read_tree.rs
@@ -1,13 +1,13 @@
 use argh::FromArgs;
-use baby_rgit::{cache::Sha256Output, cmd::error_exit};
+use baby_rgit::{cache::SHA256Output, cmd::error_exit};
 use baby_rgit::{
-    cache::{hex_to_sha256, DbEnv},
+    cache::{hex_to_sha256, DBEnv},
     cmd::db_environment,
 };
 
 fn main() {
     let ReadTree { key } = argh::from_env();
-    let db_env = DbEnv::new(db_environment());
+    let db_env = DBEnv::new(db_environment());
     if let Err(e) = db_env.unpack(&key) {
         error_exit(e);
     }
@@ -18,9 +18,9 @@ fn main() {
 struct ReadTree {
     #[argh(positional, from_str_fn(key_from_str))]
     /// SHA256 key of a commit.
-    key: Sha256Output,
+    key: SHA256Output,
 }
 
-fn key_from_str(str: &str) -> Result<Sha256Output, String> {
+fn key_from_str(str: &str) -> Result<SHA256Output, String> {
     hex_to_sha256(str).ok_or_else(|| "argument is not valid hex string".to_string())
 }
